a4a55c30fd202edca2a0300919a9807be94254e8
hanson1218 <853051727@qq.com>
2019-12-21 10:40:25 +0800

    init

---
+++ day1/day1.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+import "./pack1"
+
+var age int;
+
+
+func learn(){
+	fmt.Printf("learn go")
+	var a string = "Runoob"
+	fmt.Printf(a)
+}
+
+func main() {
+    fmt.Printf("hello, world\n")
+	learn()
+	subClass.Pack1()
+}
---
+++ pack1/pack1.go
@@ -0,0 +1,7 @@
+package subClass
+
+import "fmt"
+
+func Pack1(){
+	fmt.Printf("pack1 run")
+}
